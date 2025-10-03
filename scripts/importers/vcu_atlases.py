9c96c06fccc7b6474d06a9513cb3cebcf2dd39c2
Mike O'Brien <mike@obrien.page>
2025-10-03 16:57:38 -0400

    add baist

--- scripts/importers/vcu_atlases.py
+++ scripts/importers/vcu_atlases.py
@@ -1,17 +1,27 @@
+#!/usr/bin/env -S uv run --script
+#
 # /// script
 # dependencies = [
-#   "requests",
-#   "time",
+#   "requests"
 # ]
 # ///
 
+import argparse
 import os
-import requests
 import re
+import requests
 import time
 
+# NOTE! It just so happens that the Beers and Baist Atlases have the actual maps
+#  on pages 5-25. This is hardcoded in, but should be changed if the script is
+#  adapted for other sources.
+
+parser = argparse.ArgumentParser(description="Download Beers or Baist Atlas images from VCU and create index.")
+parser.add_argument('--atlas', type=str, default="beers", help="Which atlas to download (beers or baist)")
+args = parser.parse_args()
+
 # Create directory if it doesn't exist
-directory_path = "beers_atlas_rva"
+directory_path = f"{args.atlas}_atlas_rva"
 
 if not os.path.exists(directory_path):
     os.makedirs(directory_path)
@@ -19,7 +29,7 @@ if not os.path.exists(directory_path):
 
     if not os.path.exists(f"{directory_path}/{directory_path}_filelist.csv"):
       with open(f"{directory_path}/{directory_path}_filelist.csv", "w") as f:
-        f.write("path,section\n")
+        f.write("path,id\n")
       print(f"Index '{directory_path}_filelist.csv' created.")
 else:
     print(f"Directory '{directory_path}' already exists.")
@@ -32,37 +42,44 @@ else:
 
 
 # Download images
-beers_base = "https://scholarscompass.vcu.edu/context/beers_images/article"
-
+atlas_base = f"https://scholarscompass.vcu.edu/context/{args.atlas}_images/article"
+    
 
 for i in range(4, 25):
-    url = f"{beers_base}/10{i:02d}/type/native/viewcontent"
+    url = f"{atlas_base}/10{i:02d}/type/native/viewcontent"
     response = requests.get(
         url, 
         timeout = 30
     )
 
     if response.status_code == 200:
-        img_path = f"{directory_path}/images/beers_{i:04d}.jpg"
+        img_path = f"{directory_path}/images/{args.atlas}_{i:02d}.jpg"
         with open(img_path, "wb") as f:
             f.write(response.content)
 
         print(f"Downloaded {url} to {img_path}")
 
         item_info = requests.get(
-            url = f"https://scholarscompass.vcu.edu/beers_images/{i+1}",
+            url = f"https://scholarscompass.vcu.edu/{args.atlas}_images/{i+1}",
             headers = {
               "User-Agent": "MapRVA Georeference Tool (https://github.com/MapRVA/georeference-tool)"
             },
             timeout = 30
         )
-        item_title = re.findall(r'<meta itemprop="name" content="(.*?)"',
-                                item_info.text)
+        
+        item_id = re.findall(
+            r'Plate Number</h2>\s*<p>(.*?)</p>',
+            item_info.text
+        )
+
+        if not item_id:
+            item_id = "key"
+        else:
+            item_id = item_id[0]
 
 
-        with open(
-            f"{directory_path}/{directory_path}_filelist.csv", "a") as f:
-            f.write(f'{img_path},{re.sub(r".*_", "", item_title[0])}\n')
+        with open(f"{directory_path}/{directory_path}_filelist.csv", "a") as f:
+            f.write(f'{img_path},{item_id}\n')
         
         print(f"Added metadata to {directory_path}/{directory_path}_filelist.csv")
         
