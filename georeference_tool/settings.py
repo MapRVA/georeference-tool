1b5255ef3b3d9e8519d12839a369608586f2fc0a
Jacob Hall <email@jacobhall.net>
2025-11-10 03:36:59 -0500

    initial commit adding OpenHistoricalMap

--- georeference_tool/settings.py
+++ georeference_tool/settings.py
@@ -153,6 +153,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Whitenoise configuration for static file serving in production
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
