48b646625d3b5552c357992ebd2ca38a265f614c
Jacob Hall <email@jacobhall.net>
2026-01-20 10:16:24 -0300

    move from above georeference interface styles to separate file

--- assets/js/pages/from_above_georeference_interface.js
+++ assets/js/pages/from_above_georeference_interface.js
@@ -4,6 +4,7 @@
  */
 
 // CSS imports
+import "../../styles/pages/from-above-georeference-interface.css";
 import "maplibre-gl/dist/maplibre-gl.css";
 import "@geoman-io/maplibre-geoman-free/dist/maplibre-geoman.css";
 import "../../styles/components/image-viewer.css";
