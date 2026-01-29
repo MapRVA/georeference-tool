e11350b3378c8991109d41b1bbac805068c64579
Jacob Hall <email@jacobhall.net>
2026-01-29 01:37:19 -0500

    fix bug in cache settings

--- yesterdays/settings.py
+++ yesterdays/settings.py
@@ -211,7 +211,7 @@ else:
             "BACKEND": "django.core.files.storage.FileSystemStorage",
         },
         "staticfiles": {
-            "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+            "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
         },
     }
 
